6a134e383fd443d4db82e8a7e0e7a0e2ec45220c
Mylo James <mjames@appacademy.io>
2021-01-02 12:03:53 -0800

    doing build on before deploy

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
