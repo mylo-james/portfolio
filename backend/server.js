d686f54eda2d4dcc87f90af410273052c955e39e
Zachary Henderson <zachary.henderson114@gmail.com>
2021-01-18 23:21:56 -0600

    firebase

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.use(express.static(path.resolve('../frontend/build')));
+app.use(express.static(path.resolve("../frontend/build")));
 
-app.get('*', (req, res) => {
-    path.resolve(__dirname, '../frontend', 'build', 'index.html');
+app.get("*", (req, res) => {
+  path.resolve(__dirname, "../frontend", "build", "index.html");
 });
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
+  console.log(`Server is up on port ${port}!`);
 });
